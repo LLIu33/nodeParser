8c39a12c1327d7b73aba87d4b506395ab1e99574
tuocs <artem@intspirit.com>
2015-06-25 21:11:39 +0300

    Added parsing mytogo(not full). Added to gitignore logs and jscsrc.

--- index.js
+++ index.js
@@ -1,13 +1,101 @@
-var express = require('express');
-var app = express();
+'use strict';
+var select = require('soupselect').select;
+var htmlparser = require('htmlparser');
+var _ = require('underscore');
+var http = require('http');
+var sys = require('sys');
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+// Fetch some HTML...
+var http = require('http');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var request = http.request({
+  port: 80,
+  host: 'www.mytogo.ru',
+  method: 'GET',
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+request.on('response', function(response) {
+  response.setEncoding('utf8');
+
+  var body = '';
+  response.on('data', function(chunk) {
+    body = body + chunk;
+  });
+
+  response.on('end', function() {
+
+    // Now we have the whole body, parse it and select the nodes we want...
+    var handler = new htmlparser.DefaultHandler(function(err, dom) {
+      if (err) {
+        sys.debug('Error: ' + err);
+      } else {
+        // Get collection of taverns with kicker tables
+        var taverns = [];
+        var tavernSelector = '.container .col-lg-9.col-md-9.col-sm-8';
+        var tavernRows = select(dom, tavernSelector)[0].children;
+        tavernRows = _.filter(tavernRows, function(item) {
+          if (item.type == 'tag')
+                return item;
+        });
+        // Remove trash node from collection
+        tavernRows.shift();
+
+        _.each(tavernRows, function(tavernRow, tavernKey) {
+          // Create item for every tavern
+          taverns.push(new Tavern);
+
+          // Get name and logo
+          var tavernNode = select(tavernRow, 'img')[0];
+          taverns[tavernKey]['name'] = tavernNode.attribs.alt;
+          taverns[tavernKey]['logo'] = tavernNode.attribs.src;
+
+          // Get people activity
+          var usersNode = select(tavernRow, '.col-lg-6.col-md-6.col-sm-6');
+          var usersInside = usersNode[0].children[1];
+          var usersToPlay = usersNode[1].children[1];
+
+          _.each(usersInside.children, function(item, i) {
+            if (item.name == 'img') {
+              var userInside = new User;
+              var nameIndex = i + 1;
+              userInside['avatar'] = usersInside.children[i].attribs.src;
+              userInside['name'] = usersInside.children[nameIndex].raw;
+              taverns[tavernKey]['usersInside'].push(userInside);
+            }
+          });
+
+          _.each(usersToPlay.children, function(item, i) {
+            if (item.attribs && item.attribs.class == 'bold') {
+              console.log(item);
+              var userToPlay = new User;
+              var imgIndex = i + 2;
+              var nameIndex = i + 3;
+              userToPlay['dateOfPlan'] = item.raw;
+              userToPlay['avatar'] = usersToPlay.children[imgIndex].attribs.src;
+              userToPlay['name'] = usersToPlay.children[nameIndex].raw;
+              taverns[tavernKey]['usersPlanToPlay'].push(userToPlay);
+            }
+          });
+        });
+        // console.log(taverns[1]);
+      }
+    });
+
+    var parser = new htmlparser.Parser(handler);
+    parser.parseComplete(body);
+  });
 });
+request.end();
+
+function Tavern() {
+  this.name = '';
+  this.logo = '';
+  this.usersInside = [];
+  this.usersPlanToPlay = [];
+}
+
+function User() {
+  this.name = '';
+  this.avatar = '';
+  this.dateOfPlan = '';
+}
