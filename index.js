a92dc2d8786f808e323427f83461fd8d97594876
tuocs <artem@intspirit.com>
2015-07-02 18:06:00 +0300

    Changed port to 80

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.get('/', function(req, res, next) {
   request.end();
 });
 
-var server = app.listen(8082, function() {
+var server = app.listen(80, function() {
 
   var host = server.address().address;
   var port = server.address().port;
